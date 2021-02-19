2a5375c40714fd05640f12443b0ae058aa86e665
d4v1d41 <davepenott@protonmail.com>
2021-02-19 17:40:09 -0300

    Fix color's built contract import and apply consistency fixes

--- src/components/App.js
+++ src/components/App.js
@@ -1,52 +1,53 @@
 import React, { Component } from 'react';
 import Web3 from 'web3'
 import './App.css';
-import Color from '../abis/Color.json'
+import Color from '../contracts/build/Color.json';
 
 class App extends Component {
 
   async componentWillMount() {
-    await this.loadWeb3()
-    await this.loadBlockchainData()
+    await this.loadWeb3();
+    await this.loadBlockchainData();
   }
 
   async loadWeb3() {
     if (window.ethereum) {
-      window.web3 = new Web3(window.ethereum)
-      await window.ethereum.enable()
+      window.web3 = new Web3(window.ethereum);
+      await window.ethereum.enable();
     }
     else if (window.web3) {
-      window.web3 = new Web3(window.web3.currentProvider)
+      window.web3 = new Web3(window.web3.currentProvider);
     }
     else {
-      window.alert('Non-Ethereum browser detected. You should consider trying MetaMask!')
+      window.alert('Non-Ethereum browser detected. You should consider trying MetaMask!');
     }
   }
 
   async loadBlockchainData() {
-    const web3 = window.web3
+    const web3 = window.web3;
     // Load account
-    const accounts = await web3.eth.getAccounts()
-    this.setState({ account: accounts[0] })
+    const accounts = await web3.eth.getAccounts();
+    this.setState({ account: accounts[0] });
 
-    const networkId = await web3.eth.net.getId()
-    const networkData = Color.networks[networkId]
+    const networkId = await web3.eth.net.getId();
+    const networkData = Color.networks[networkId];
+
     if(networkData) {
-      const abi = Color.abi
-      const address = networkData.address
-      const contract = new web3.eth.Contract(abi, address)
-      this.setState({ contract })
-      const totalSupply = await contract.methods.totalSupply().call()
-      this.setState({ totalSupply })
+      const abi = Color.abi;
+      const address = networkData.address;
+      const contract = new web3.eth.Contract(abi, address);
+      this.setState({ contract });
+      const totalSupply = await contract.methods.totalSupply().call();
+      this.setState({ totalSupply });
       // Load Colors
       for (var i = 1; i <= totalSupply; i++) {
-        const color = await contract.methods.colors(i - 1).call()
+        const color = await contract.methods.colors(i - 1).call();
         this.setState({
           colors: [...this.state.colors, color]
-        })
+        });
       }
     } else {
-      window.alert('Smart contract not deployed to detected network.')
+      window.alert('Smart contract not deployed to detected network.');
     }
   }
 
@@ -55,8 +56,8 @@ class App extends Component {
     .once('receipt', (receipt) => {
       this.setState({
         colors: [...this.state.colors, color]
-      })
-    })
+      });
+    });
   }
 
   constructor(props) {
