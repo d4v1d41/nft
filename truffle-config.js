88dafb5b20e25c66b3b21355482abb94ddf4a810
d4v1d41 <davepenott@protonmail.com>
2021-02-19 17:39:09 -0300

    Move build contracts folder to /src/contracts/build

--- truffle-config.js
+++ truffle-config.js
@@ -10,7 +10,7 @@ module.exports = {
     },
   },
   contracts_directory: './src/contracts/',
-  contracts_build_directory: './src/abis/',
+  contracts_build_directory: './src/contracts/build',
   compilers: {
     solc: {
       optimizer: {
